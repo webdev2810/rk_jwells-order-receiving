d3295e96021e70c3c4a3d8edcc4b03e02e3843c9
webdev2810 <138306196+webdev2810@users.noreply.github.com>
2026-02-26 15:13:08 +0530

    first commit

--- models/OrderCancel.js
+++ models/OrderCancel.js
@@ -1,26 +1,20 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const OrderCancelSchema = new Schema({
-    
-    email:{
-        type: String,
-        required: true,
-        // unique: true
-    },
-    phone:{
-        type: String,
-        required: true
-    },
-    // productId:{
-    //     type: String,
-    //     required: true
-    // },
-    Date:{
-        type: Date,
-        default: Date.now
-    },
-  });
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-  const OrderCancel = mongoose.model('ordercancel',OrderCancelSchema);
-  module.exports = OrderCancel;
+const OrderCancel = mongoose.model("ordercancel", OrderCancelSchema);
+module.exports = OrderCancel;
--- models/OrderReturn.js
+++ models/OrderReturn.js
@@ -1,25 +1,20 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const OrderReturnSchema = new Schema({
-    
-    email:{
-        type: String,
-        required: true,
-    },
-    phone:{
-        type: String,
-        required: true
-    },
-    // productId:{
-    //     type: String,
-    //     required: true
-    // },
-    Date:{
-        type: Date,
-        default: Date.now
-    },
-  });
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-  const OrderReturn = mongoose.model('orderreturn',OrderReturnSchema);
-  module.exports = OrderReturn;
+const OrderReturn = mongoose.model("orderreturn", OrderReturnSchema);
+module.exports = OrderReturn;
